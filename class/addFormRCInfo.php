6276268294542d13d88cc76c340de88b7f8696f1
Gil Christiano Guedes dos Santos <gilchristiano@yahoo.com.br>
2015-06-16 10:44:29 -0300

    Alterando nome da tabela
    de capacitacao para Rel_Cap_Qua
    Relacionamento Capacitação e Qualificação

--- class/addFormRCInfo.php
+++ class/addFormRCInfo.php
@@ -8,7 +8,7 @@ function __construct() {
 	mysql_select_db($db,$this->base);} // fim do construtor de classe
 
 function addFormRCInfo($name,$surname,$career,$occupation,$workload,$institution_name,$type_instituition,$level_of_education,$level_incomplete_education,$knowledge_area,$finality,$modality,$relationship_with_objectives,$observation){
-	$sql = "insert into capacitacao(name,surname,career,occupation,workload,institution_name,type_instituition,level_of_education,level_incomplete_education,knowledge_area,finality,modality,relationship_with_objectives,observation)"
+	$sql = "insert into Rel_Cap_Qua(name,surname,career,occupation,workload,institution_name,type_instituition,level_of_education,level_incomplete_education,knowledge_area,finality,modality,relationship_with_objectives,observation)"
 	$sql .= "values ('$name','$surname','$career','$occupation','$workload','$institution_name','$type_instituition','$level_of_education','$level_incomplete_education','$knowledge_area','$finality','$modality','$relationship_with_objectives','$observation')";
 	$result = mysql_query($sql,$this->base);
 	return $result;}
