e2c022a5194ddbd56834c5624044765e30001fb7
Gil Christiano Guedes dos Santos <gilchristiano@yahoo.com.br>
2015-06-16 11:04:16 -0300

    Inserção de variaveis de campo
    Conexões para cadastros

--- connection/addFormRCInfo.php
+++ connection/addFormRCInfo.php
@@ -5,28 +5,28 @@
 // Seção de conexão com a base de dados.
 // Campos relativos aos cadastros
 
-	$name = $_post[]; // nome
-	$surname = $_POST[]; // sobrenome
-	$career = $_POST[]; // Carreira
-	$occupation = $_POST[]; // Ocupação
-	$workload = $_POST[]; // Carga Horária
-	$institution_name = $_POST[]; // Nome da instituição
-	$type_instituition = $_POST[]; // Tipo da instituição
-	$level_of_education = $_POST[]; // Nível de escolaridade
-	$level_incomplete_education = $_POST[]; // Nível de escolaridade em andamento
-	$knowledge_area = $_POST[]; // Área de conhecimento
-	$finality = $_POST[]; // Finalidade (motivo)
-	$modality = $_POST[]; // Modalidade
-	$relationship_with_objectives =  $_POST[]; //Relação com os objetivos estratégicos
-	$Observation = $_POST[]; // Observações
+	$name = $_post['name']; // nome
+	$surname = $_POST['surname']; // sobrenome
+	$career = $_POST['career']; // Carreira
+	$occupation = $_POST['occupation']; // Ocupação
+	$workload = $_POST['workload']; // Carga Horária
+	$institution_name = $_POST['institution_name']; // Nome da instituição
+	$type_instituition = $_POST['type_instituition']; // Tipo da instituição
+	$level_of_education = $_POST['level_of_education']; // Nível de escolaridade
+	$level_incomplete_education = $_POST['level_incomplete_education']; // Nível de escolaridade em andamento
+	$knowledge_area = $_POST['knowledge_area']; // Área de conhecimento
+	$finality = $_POST['finality']; // Finalidade (motivo)
+	$modality = $_POST['modality']; // Modalidade
+	$relationship_with_objectives =  $_POST['relationship_with_objectives']; //Relação com os objetivos estratégicos
+	$observation = $_POST['observation']; // Observações
 	
 /* As informações específicas para cadastros nos campos estão
 guardadas em base de dados e são configuradas pelo gestor
 da aplicação! */
-
+ /*
 	$interface_confirm = "$name, $surname, $career, $occupation, $workload, $institution_name, $type_instituition, $level_of_education,";
-	$interface_confirm .= "$level_incomplete_education, $knowledge_area, $finality, $modality, $relationship_with_objectives, $Observation";
-	
+	$interface_confirm .= "$level_incomplete_education, $knowledge_area, $finality, $modality, $relationship_with_objectives, $observation";
+*/
 	include "../classes/addFormRCInfo.php"
 	
 	$objAddInfo = new FormRCInfo.php();
